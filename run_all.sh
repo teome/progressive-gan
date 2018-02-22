fa54abe05b964f0d9fb0e0813a8a9f6ea6433d46
Dom Kelly <domkelly1010@gmail.com>
2018-02-22 23:05:35 +0000

    Add convenience scripts

---
+++ run_all.sh
@@ -0,0 +1,19 @@
+# ds_root='/home/mval/datasets/cyclegan_celeb/origs/street_grey_3/'
+dt_dir='/scratch/progressive_0'
+ds_root=$dt_dir'/data'
+chkp_dir='/home/mval/data/progressive_0/logs_0/set_0'
+proj_dir=$dt_dir'/code'
+machine='mval2.mval'
+max_stage=8
+iters=$((601600*10))
+echo $iters
+
+sleep 1.1; python cv_condor.py --project-dir $proj_dir --checkpoints-dir $chkp_dir --machine $machine --num-gpus 1 --killer --niter $iters --batchSize 64 --niter-decay 0 --fineSize 32 --loadSize 32 --workers 10 --outf $chkp_dir --dataroot $ds_root --lr 0.0002 --beta1 0.5 --ngpu 1 --beta2 0.999 --max-stage $max_stage --stage-interval 300800 --print-iter-freq 47 --train-log-freq 47 --model-save-freq 235 --image-save-freq 235 --auto-continue
+sleep 1.1; python cv_condor.py --project-dir $proj_dir --checkpoints-dir $chkp_dir --machine $machine --num-gpus 1 --killer --niter $iters --batchSize 64 --niter-decay 0 --fineSize 32 --loadSize 32 --workers 10 --outf $chkp_dir --dataroot $ds_root --lr 0.0004 --beta1 0.5 --ngpu 1 --beta2 0.999 --max-stage $max_stage --stage-interval 300800 --print-iter-freq 47 --train-log-freq 47 --model-save-freq 235 --image-save-freq 235 --auto-continue
+sleep 1.1; python cv_condor.py --project-dir $proj_dir --checkpoints-dir $chkp_dir --machine $machine --num-gpus 1 --killer --niter $iters --batchSize 64 --niter-decay 0 --fineSize 32 --loadSize 32 --workers 10 --outf $chkp_dir --dataroot $ds_root --lr 0.0020 --beta1 0.5 --ngpu 1 --beta2 0.999 --max-stage $max_stage --stage-interval 300800 --print-iter-freq 47 --train-log-freq 47 --model-save-freq 235 --image-save-freq 235 --auto-continue
+
+
+
+# export NV_GPU=$CUDA_VISIBLE_DEVICES
+# cat /etc/hostname
+# exec /usr/bin/nvidia-docker run --rm --ipc=host --sig-proxy=true --volume /scratch/progressive_0/code:/scratch/progressive_0/code --volume /home/mval:/home/mval --volume /scratch:/scratch --workdir /scratch/progressive_0/code --user 1002:999 vj/pytorch:gan python /scratch/progressive_0/code/train.py --auto-continue --batchSize 64 --beta1 0.5 --beta2 0.99 --outf /home/mval/data/progressive_0/logs_0/set_0 --dataroot /scratch/progressive_0/data/ --fineSize 64 --killer --loadSize 64 --lr 0.0002 --manualSeed 0 --max-stage 8 --name model_20180221-210000_0 --niter 47000 --niter-decay 0 --ngpu 1 --phase train --print-iter-freq 47 --train-log-freq 47 --model-save-freq 235 --image-save-freq 235 --stage-interval 300800
---
+++ run_tmp.sh
@@ -0,0 +1,1 @@
+python train.py --killer --niter 94000 --batchSize 64 --fineSize 64 --loadSize 64 --workers 10 --ngpu 1 --outf /home/mval/data/progressive_1/logs_0/model_20180222_100002_0 --dataroot /scratch/progressive_0/data --stage-interval 601600 --max-stage 8 --print-iter-freq 47 --train-log-freq 47 --model-save-freq 470 --image-save-freq 94 --lr 0.0010
