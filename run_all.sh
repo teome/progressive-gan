ace13f5d5a937188c7dea7c17299448f1aba1fcb
Dom Kelly <domkelly1010@gmail.com>
2018-02-23 15:45:11 +0000

    Fixes

--- run_all.sh
+++ run_all.sh
@@ -1,16 +1,16 @@
 # ds_root='/home/mval/datasets/cyclegan_celeb/origs/street_grey_3/'
 dt_dir='/scratch/progressive_0'
 ds_root=$dt_dir'/data'
-chkp_dir='/home/mval/data/progressive_0/logs_0/set_0'
+chkp_dir='/home/mval/data/progressive_1/logs_0/'
 proj_dir=$dt_dir'/code'
 machine='mval2.mval'
 max_stage=8
-iters=$((601600*10))
+iters=$((601600*14))
 echo $iters
 
-sleep 1.1; python cv_condor.py --project-dir $proj_dir --checkpoints-dir $chkp_dir --machine $machine --num-gpus 1 --killer --niter $iters --batchSize 64 --niter-decay 0 --fineSize 32 --loadSize 32 --workers 10 --outf $chkp_dir --dataroot $ds_root --lr 0.0002 --beta1 0.5 --ngpu 1 --beta2 0.999 --max-stage $max_stage --stage-interval 300800 --print-iter-freq 47 --train-log-freq 47 --model-save-freq 235 --image-save-freq 235 --auto-continue
-sleep 1.1; python cv_condor.py --project-dir $proj_dir --checkpoints-dir $chkp_dir --machine $machine --num-gpus 1 --killer --niter $iters --batchSize 64 --niter-decay 0 --fineSize 32 --loadSize 32 --workers 10 --outf $chkp_dir --dataroot $ds_root --lr 0.0004 --beta1 0.5 --ngpu 1 --beta2 0.999 --max-stage $max_stage --stage-interval 300800 --print-iter-freq 47 --train-log-freq 47 --model-save-freq 235 --image-save-freq 235 --auto-continue
-sleep 1.1; python cv_condor.py --project-dir $proj_dir --checkpoints-dir $chkp_dir --machine $machine --num-gpus 1 --killer --niter $iters --batchSize 64 --niter-decay 0 --fineSize 32 --loadSize 32 --workers 10 --outf $chkp_dir --dataroot $ds_root --lr 0.0020 --beta1 0.5 --ngpu 1 --beta2 0.999 --max-stage $max_stage --stage-interval 300800 --print-iter-freq 47 --train-log-freq 47 --model-save-freq 235 --image-save-freq 235 --auto-continue
+sleep 1.1; python cv_condor.py --project-dir $proj_dir --checkpoints-dir $chkp_dir --machine $machine --num-gpus 2 --killer --niter $iters --batchSize 64 --niter-decay 0 --fineSize 64 --loadSize 64 --workers 10 --outf $chkp_dir --dataroot $ds_root --lr 0.0002 --beta1 0.5 --ngpu 1 --beta2 0.999 --max-stage $max_stage --stage-interval 300800 --print-iter-freq 47 --train-log-freq 47 --model-save-freq 235 --image-save-freq 235 --auto-continue
+sleep 1.1; python cv_condor.py --project-dir $proj_dir --checkpoints-dir $chkp_dir --machine $machine --num-gpus 2 --killer --niter $iters --batchSize 64 --niter-decay 0 --fineSize 64 --loadSize 64 --workers 10 --outf $chkp_dir --dataroot $ds_root --lr 0.0004 --beta1 0.5 --ngpu 1 --beta2 0.999 --max-stage $max_stage --stage-interval 300800 --print-iter-freq 47 --train-log-freq 47 --model-save-freq 235 --image-save-freq 235 --auto-continue
+sleep 1.1; python cv_condor.py --project-dir $proj_dir --checkpoints-dir $chkp_dir --machine $machine --num-gpus 2 --killer --niter $iters --batchSize 64 --niter-decay 0 --fineSize 64 --loadSize 64 --workers 10 --outf $chkp_dir --dataroot $ds_root --lr 0.0020 --beta1 0.5 --ngpu 1 --beta2 0.999 --max-stage $max_stage --stage-interval 300800 --print-iter-freq 47 --train-log-freq 47 --model-save-freq 235 --image-save-freq 235 --auto-continue
 
 
 
